0a9b83924d2d9bc49a97cb58495a875fa38b6552
BjBodner <benjybo7@gmail.com>
2020-03-21 16:04:29 +0200

    fixed up the transition manager

--- time_counter.py.py
+++ time_counter.py.py
@@ -1,8 +1,10 @@
 
 
-class DeseaseStageTransitions:
+class TransitionManager:
 
-    def __init__(self):
+    def __init__(self, num_points):
+
+        self.num_points = num_points
 
         self.transition_times = {
             "infected-contagious": {"mean": 20, "std": 2},
@@ -11,68 +13,86 @@ class DeseaseStageTransitions:
             "immobilized-recovered": {"mean" : 50, "std": 2}
         }
 
+        self.number_of_transitions = len(self.transition_times)
+        self.time_counters = np.zeros(num_points)
+        self._set_transition_dicts()
+        self._sample_transition_times_from_distribution()
+
+
+    def _set_transition_dicts(self):
+        """ initializes the transition dicts"""
         self.transition_2_num = {
-            "infected-contagious": 0,
-            "contagious-diagnosis": 1,
-            "diagnosis-immobilized": 2,
-            "immobilized-recovered": 3
+            "infected-contagious": 1,
+            "contagious-diagnosis": 2,
+            "diagnosis-immobilized": 3,
+            "immobilized-recovered": 4
         }
 
         self.num_2_transition = {
-            0 : "infected-contagious",
-            1 : "contagious-diagnosis",
-            2 : "diagnosis-immobilized",
-            3 : "immobilized-recovered",
+            1 : "infected-contagious",
+            2 : "contagious-diagnosis",
+            3 : "diagnosis-immobilized",
+            4 : "immobilized-recovered",
         }
-    
-    def get_transition_times(self):
-        """returns the parameters for the distribution of transitions, between different stages of the desease
+
+
+   def _sample_transition_times_from_distribution(self):
+        """ samples the transition times for all individuals, and saves in self.transition_times """
+        transition_array = np.zeros((self.num_points , self.number_of_transitions))
+        for i in range(1, self.number_of_transitions):
+
+            # get distribution parameters
+            mean = self.transition_times[ self.num_2_transition [i] ] ["mean"]
+            std = self.transition_times[ self.num_2_transition [i] ] ["std"]
+            
+            # sample transition times from distribution
+            transition_array[: , i] = std * np.random.randn(self.num_points) + mean
+        
+        self.transition_times = transition_array
+
+
+    def update_stages_of_desease(self, current_stages_of_individuals):
+        """ updates the stages of the desease"""
+
+        # updates the stages of the individuals
+        new_stages_of_individuals, indicies_that_transitioned_to_next_stage = self.update_stages(current_stages_of_individuals)
+
+        # increments the time counter for individuals, and reboots it if a transition occured
+        if len(indicies_of_counters_to_reboot) > 0
+            self.time_counters[indicies_of_counters_to_reboot] = 0
+        self.time_counters += 1
+
+        return new_stages_of_individuals
+
+
+    def update_stages(self, current_stages_of_individuals):
+        """updates the stages of the desease for all individuals, using the transition times
+        
+        Arguments:
+            current_stages_of_individuals {[np.ndarray]} -- the current stages of all individuals
         
         Returns:
-            [dict] -- the transition time between different stages of the desease 
-                        keys = [infected-contagious, contagious-diagnosis, diagnosis-immobilized, immobilized-recovered]
+            [np.ndarray, np.ndarray] -- [the new stages of the individuals, the indicies for which these changes happened]
         """
-        return self.transition_times
+        # get the relavent transition times for each individual
+        current_stages_of_individuals_ = np.expand_dims(current_stages_of_individuals, 1)
+        relavent_transition_times = self.transition_times[stagecurrent_stages_of_individuals_s_of_individuals_ ]
 
-    def get_transition_2_num_dict(self):
-        """returns a dict whith keys=transition names, and values=transition num id
-        
-        Returns:
-            [dict] -- a dict to covert the transition id to its corresponding number id
-                        keys = [infected-contagious, contagious-diagnosis, diagnosis-immobilized, immobilized-recovered]
-        """
-        return self.transition_2_num
+        # find transitions to apply
+        transitions_to_apply = (self.time_counters > relavent_transition_times).int()
 
+        # apply transitions and get the indicies which they occurs in
+        new_stages_of_individuals = current_stages_of_individuals + transitions_to_apply
+        indicies_that_transitioned_to_next_stage = np.nonzero(transitions_to_apply) [0] 
 
-    def get_transition_2_num_dict(self):
-        """returns a dict whith keys=transition num id, and values=transition names
-        
-        Returns:
-            [dict] -- a dict to covert the transition number id to its corresponding name
-                        keys = [0,1,2,3]
-        """
-        return self.num_2_transition
+        return new_stages_of_individuals, indicies_that_transitioned_to_next_stage
 
 
-    def sample_transition_times(self):
-
-        
-    
 
 
-class TimeCounter:
+def test_transition_manager():
+    transition_manager = TransitionManager()
 
 
-    def __init__(self, num_points):
-
-        self.num_points = num_points
-        self.time_counters = np.zeros(num_points)
-        self.transition_times = get_transition_times()
-        
-
-    def get_time_counters_of_all_individuals(self):
-
-
-    def get_transition_times(self):
-        time_counters = self.get_time_counters_of_all_individuals()
-        transition_times = self.get_transition_times()
+if __name__ == '__main__':
+    test_transition_manager()
